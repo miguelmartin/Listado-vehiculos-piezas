50706923ec4720bb9d0f78ea2464e215f5cac8de
root <root@debian.gonzalonazareno.org>
2013-03-05 10:13:36 +0100

    Extraer y mostrar por pantalla datos del formulario html

---
+++ obtener-datos-formhtml.py
@@ -0,0 +1,8 @@
+
+# Asignamos variables python a las variables del formulario
+marca_coche = form["marca"].value
+modelo_marca = form["modelo"].value
+# Imprimimos las variales seleccionadas
+print marca_coche
+print modelo_marca
+
