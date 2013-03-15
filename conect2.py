e10307eb3eaf413e13c08d11ae7df72c32cd186b
miguelmartin <miguegt@hotmail.es>
2013-03-15 17:22:52 +0100

    solicitamos la contraseña para no dejarla en plano

--- conect2.py
+++ conect2.py
@@ -2,8 +2,10 @@
 import psycopg2
 import sys
 import pprint
+from getpass import getpass
+contrasenna = getpass("Contrasenna: ")
 def main():
-	conn_string = "host='localhost' dbname='prueba' user='postgres' password='usuario'"
+	conn_string = "host='localhost' dbname='prueba' user='postgres' password='%S'" % contrasenna
  	conn = psycopg2.connect(conn_string)
  	cursor = conn.cursor()
  	cursor.execute("select dni,telefono from clientes;")
