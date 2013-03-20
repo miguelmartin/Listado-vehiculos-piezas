bde1a77307f1f3bd90174af3ec9f81870f52874a
miguelmartin <miguegt@hotmail.es>
2013-03-20 08:32:04 +0100

    solicitamos los parametro de la consulta

--- conect2.py
+++ conect2.py
@@ -4,12 +4,19 @@ import sys
 import pprint
 from getpass import getpass
 contrasenna = getpass("Contrasenna: ")
+
+marca = raw_input("Introduce la marca: ")
+modelo = raw_input("Introduce el modelo: ")
+anno = raw_input("Introduce el año: ")
+combustible = raw_input("Introduce el combustible: ")
+
 def main():
-	conn_string = "host='localhost' dbname='prueba' user='postgres' password='%S'" % contrasenna
- 	conn = psycopg2.connect(conn_string)
- 	cursor = conn.cursor()
- 	cursor.execute("select dni,telefono from clientes;")
- 	records = cursor.fetchall()
- 	pprint.pprint(records)
+        conn_string = "host='' dbname='' user='' password='%s'" % contrasenna
+        conn = psycopg2.connect(conn_string)
+        cursor = conn.cursor()
+        cursor.execute("select nombreversion from vehiculos where upper(nombreversion) like "+"'%"+marca+"%' and upper(nombreversion) like "+"'%"+mod$
+        records = cursor.fetchall()
+        pprint.pprint(records)
 if __name__ == "__main__":
-	main()
+        main()
+
