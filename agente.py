32e6a9ef7c84e895bbc04abe8cc1b423482cb318
miguelmartin <miguegt@hotmail.es>
2013-04-17 10:31:44 +0300

    cada vehiculo con su imagen

--- agente.py
+++ agente.py
@@ -21,16 +21,13 @@ def main():
         cursor = conn.cursor()
         cursor.execute("select distinct(v.idvehiculo),v.nombreversion from vehiculos v,albumvehiculos a,ficheros fi,datosficheros f where v.idvehiculo=a.idvehiculo and fi.id=a.idficherofoto and f.masterid=a.idficherofoto and upper(nombreversion) like "+"'%"+marca.upper()+"%' and upper(nombreversion) like "+"'%"+modelo.upper()+"%' and añoversion like "+"'%"+anno+"%' and upper(otrocombustible) like "+"'%"+combustible.upper()+"%';")
         resultado = cursor.fetchall()
-        cursor2 = conn.cursor()
-        cursor2.execute("select f.datos from vehiculos v,albumvehiculos a,ficheros fi,datosficheros f where v.idvehiculo=a.idvehiculo and fi.id=a.idficherofoto and f.masterid=a.idficherofoto and v.idvehiculo='%s';" %resultado[0][0]) 
-        for mypic2 in cursor2.fetchall():
-                open("/var/www/img/"+str(resultado[0][0])+".jpg", 'wb').write(str(mypic2[0]))
-	        print "<ul>"
-	        print "<li><img src='/img/"+str(resultado[0][0])+".jpg' alt='Smiley face' height='60' width='60'>'%s'</li>" %resultado[0][1]
-	        print "</ul>"
-	        print "<form method='post' action='/cgi-bin/agente2.py'> Ingresa el id: <input name='idvehiculo' type='text' value='idvehiculo' /><input type='submit' value='Consultar' /></form> "
-	
-        if __name__ == "__main__":
+	print "<ul>"
+	for coche in resultado:
+		cursor2 = conn.cursor()
+        	cursor2.execute("select f.datos from vehiculos v,albumvehiculos a,ficheros fi,datosficheros f where v.idvehiculo=a.idvehiculo and fi.id=a.idficherofoto and f.masterid=a.idficherofoto and v.idvehiculo="+str(coche[0])+";")
+        	mypic2 = cursor2.fetchone()
+                open("/var/www/img/"+str(coche[0])+".jpg", 'wb').write(str(mypic2[0]))
+		print "<il><img src='/img/"+str(coche[0])+".jpg' alt='Smiley face' height='60' width='60'>"+(coche[1])+"</il></br>"
+	print "</ul>"	
+if __name__ == "__main__":
                 main()
-
-
