2b095ab5669155b9d30ec2822e5507a3496e37ee
miguelmartin <miguegt@hotmail.es>
2013-04-23 18:00:03 +0300

    pieza de cada vehiculo

--- agente2.py
+++ agente2.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
+
+import cgi
+import cgitb; cgitb.enable() # Optional; for debugging only
 import psycopg2
 import sys
 import pprint
 import cgi
 
-#Obtenemos valores
-
-print "Content-Type: text/html\n"
-form = cgi.FieldStorage()
-idvehiculo = form.getvalue("idvehiculo")
+print "Content-Type: text/html"
+print ""
+arguments = cgi.FieldStorage()
+for i in arguments.keys():
+ codvehiculo =  arguments[i].value
 
 #Conexion a la base de datos y consulta
 def main():
-        conn_string = "host='' dbname='' user='' password=''"
+        conn_string = "host='192.168.1.250' dbname='crvnet5' user='augusto' password='augusto2013'"
         conn = psycopg2.connect(conn_string)
         cursor = conn.cursor()
-        cursor.execute("select art.descripcion,es.refid from entradastock es inner join referencias ref on es.referencia = ref.referencia inner join $
-        records = cursor.fetchall()
-        pprint.pprint(records)
+        cursor.execute("select art.descripcion,es.refid from entradastock es inner join referencias ref on es.referencia = ref.referencia inner join articulos art on art.idarticulo = ref.idarticulo inner join versiones ver on ver.idversion = ref.idversion where es.refid in (select refid from stockvehiculo where idvehiculo = "+str(codvehiculo)+") ;")
+        resultado = cursor.fetchall()
+        print "<img src='/img/"+str(codvehiculo)+".jpg' alt='Smiley face' height='220' width='220'></img>"
+        print "<ul>"
+        for pieza in resultado:
+               print "<a href='/cgi-bin/agente3.py?idvehiculo="+str(pieza[1])+"'><il>"+(pieza[0])+"</il></a><br/>"
+        print "</ul>"
+
 if __name__ == "__main__":
         main()
 
