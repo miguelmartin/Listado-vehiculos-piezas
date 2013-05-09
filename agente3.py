fb96e8f1aaff8a945fa77ccd2e02bcd795fa4906
miguelmartin <miguegt@hotmail.es>
2013-05-09 14:15:50 +0300

    todas las imagenes de una pieza

--- agente3.py
+++ agente3.py
@@ -8,12 +8,38 @@ import sys
 import pprint
 import cgi
 import os.path
+import subprocess
+cont = 0
+
 
 print "Content-Type: text/html"
 print ""
 arguments = cgi.FieldStorage()
 for i in arguments.keys():
 	refid =  arguments[i].value
+
+if not os.path.exists("/var/www/img/"+str(refid)+str(cont+1)+".jpg"):
+	def main():
+       		conn_string = "host='' dbname='' user='' password=''"
+       		conn = psycopg2.connect(conn_string)
+		cursor2 = conn.cursor()	
+		cursor2.execute("select f.datos from entradastock e inner join albumpiezas a on e.refid=a.refid inner join ficheros fi on fi.id=a.idficherofoto inner join datosficheros f on f.masterid=a.idficherofoto where e.refid="+str(refid)+";")
+		fotos = cursor2.fetchall()
+		cont2 = 0
+		if fotos == []:
+			print "<img src='/img/defecto.jpg' alt='Smiley face' height='100' width='100'></img>"
+		else:
+			for foto in fotos:
+				open("/var/www/img/"+str(refid)+""+str(cont2)+".jpg", 'wb').write(str(foto[0]))
+				print  "<a href='/img/"+str(refid)+str(cont2)+".jpg'><img src='/img/"+str(refid)+str(cont2)+".jpg' alt='Smiley face' height='100' width='100'></a>"	
+				cont2 += 1
+	if __name__ == "__main__":
+       		main()
+else:
+	numarchivos = subprocess.check_output("ls -l /var/www/img/"+str(refid)+"[0-9].jpg | wc -l", shell=True)
+	for x in xrange(int(numarchivos)):
+		print "<a href='/img/"+str(refid)+str(cont)+".jpg'><img src='/img/"+str(refid)+str(cont)+".jpg' alt='Smiley face' height='100' width='100'></a>"
+		cont += 1
 
 #Conexion a la base de datos y consulta
 def main():
@@ -22,19 +48,7 @@ def main():
         cursor = conn.cursor()
         cursor.execute("select referencia,estado,ubicacion,precio,metadatos from entradastock where refid="+str(refid)+" ;")
         resultado = cursor.fetchall()
-	if not os.path.exists("/var/www/img/"+str(refid)+".jpg"):
-		cursor2 = conn.cursor()
-        	cursor2.execute("select f.datos from entradastock e inner join albumpiezas a on e.refid=a.refid inner join ficheros fi on fi.id=a.idficherofoto inner join datosficheros f on f.masterid=a.idficherofoto where e.refid="+str(refid)+";")
-        	mypic2 = cursor2.fetchone()
-		if str(type(mypic2)) == "<type 'NoneType'>":
-			url = "<img src='/img/defecto.jpg' alt='Smiley face' height='300' width='300'></img>"
-		else:
-        		open("/var/www/img/"+str(refid)+".jpg", 'wb').write(str(mypic2[0]))
-			url = "<img src='/img/"+str(refid)+".jpg' alt='Smiley face' height='300' width='300'></img>"
-	else:
-		url = "<img src='/img/"+str(refid)+".jpg' alt='Smiley face' height='300' width='300'></img>"
 	print "<ul>"
-	print url
         for datopieza in resultado:
                 print "<ul>"+str(datopieza[1])+"</ul>"
                 print "<ul>"+str(datopieza[0])+"</ul>"
