76fc47d554d13608ac0c8b8209813d6e93a2a614
miguelmartin <miguegt@hotmail.es>
2013-05-20 18:59:38 +0200

    cambios

--- agente2.py
+++ agente2.py
@@ -57,7 +57,7 @@ else:
 		cont += 1
 
 def main():
-	conn_string = "host='192.168.1.250' dbname='crvnet5' user='augusto' password='augusto2013'"
+	conn_string = "host='' dbname='' user='' password=''"
         conn = psycopg2.connect(conn_string)
         cursor = conn.cursor()
         cursor.execute("select art.descripcion,es.refid from entradastock es inner join referencias ref on es.referencia = ref.referencia inner join articulos art on art.idarticulo = ref.idarticulo inner join versiones ver on ver.idversion = ref.idversion where es.refid in (select refid from stockvehiculo where idvehiculo = "+str(codvehiculo)+") ;")
